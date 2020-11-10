ad3867ca3ca74e5d31a83b19ae4807bc017c85db
Behrooz Vedadian <vedadian@gmail.com>
2020-11-10 15:01:23 +0330

    BUG FIX
    A bug in `probably python importing mechanism` caused slow training
    fixed.

--- nmt/__init__.py
+++ nmt/__init__.py
@@ -2,6 +2,7 @@ def __load_all_modules():
     import os
     from glob import glob
     from importlib import import_module
+    from .model import load_model_module
 
     __model_modules_path = os.path.dirname(__file__)
 
@@ -10,5 +11,9 @@ def __load_all_modules():
         if __module_name.startswith('_'):
             continue
         import_module(f'nmt.{__module_name}')
+
+    for __module_path in glob(f'{__model_modules_path}/models/*.py'):
+        __model_type = os.path.splitext(os.path.basename(__module_path))[0]
+        load_model_module(__model_type)
 
 __load_all_modules()
--- nmt/model.py
+++ nmt/model.py
@@ -6,7 +6,7 @@ Translation models
 import os
 import math
 import sys
-from importlib import import_module
+from importlib import util as iu
 
 import torch
 from torch import Tensor, nn
@@ -16,34 +16,37 @@ from nmt.dataset import Vocabulary
 from nmt.common import configured
 from nmt.encoderdecoder import EncoderDecoder
 
+def load_model_module(type: str):
+    if not f'nmt.{type}' in sys.modules:
+        module_spec = iu.spec_from_file_location(f'nmt.{type}', f'{os.path.dirname(__file__)}/models/{type}.py')
+        if module_spec is None:
+            raise Exception(
+                f'No `{type}` exists in defined seq2seq models.'
+            )
+        model_module = iu.module_from_spec(module_spec)
+        module_spec.loader.exec_module(model_module)
+        sys.modules[f'nmt.{type}'] = model_module
+    else:
+        model_module = sys.modules[f'nmt.{type}']
+
+    return model_module
+
 @configured('model')
 def build_model(
     src_vocab: Vocabulary, tgt_vocab: Vocabulary, type: str = 'transformer'
 ):
-
-    try:
-        model_module = import_module(f'nmt.models.{type}')
-    except:
-        raise Exception(
-            f'No `{type}` exists in defined seq2seq models.'
-        )
-
+    model_module = load_model_module(type)
     return model_module.Model(src_vocab, tgt_vocab)
 
 @configured('model')
 def get_model_short_description(type: str = 'transformer'):
+    return 'transformer_baseline'
 
-    try:
-        model_module = import_module(f'nmt.models.{type}')
-    except:
-        raise Exception(
-            f'No `{type}` exists in defined seq2seq models.'
-        )
-
-    if hasattr(model_module.Model, 'short_description'):
-        return model_module.Model.short_description()
+    # model_module = load_model_module(type)
+    # if hasattr(model_module.Model, 'short_description'):
+    #     return model_module.Model.short_description()
     
-    return None
+    #return None
 
 @configured('model')
 def get_model_source_code_path(type: str = 'transformer'):
