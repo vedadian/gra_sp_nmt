22f0f905d5fa957e809f40b4b4ce1aad7d5c343b
Behrooz Vedadian <vedadian@gmail.com>
2021-09-13 18:59:46 +0430

    Updated to enable automatic evaluation of multiple trained models

---
+++ nmt/cli_interface.py
@@ -0,0 +1,172 @@
+# coding: utf-8
+"""
+Entrypoint of command line utility
+"""
+
+import os
+import sys
+import json
+import shutil
+import argparse
+from typing import Sequence
+
+import nmt.common
+
+from nmt.common import configuration, set_mode, make_logger, logger
+from nmt.dataset import prepare_data, get_validation_dataset, get_test_dataset
+from nmt.model import get_model_short_description, get_model_source_code_path
+from nmt.train import train
+from nmt.predict import predict, evaluate, get_vocabularies
+from nmt.sanity import sanity_check
+from nmt.visualization import visualization
+
+def run_evaluate(get_dataset, log_prefix):
+    dataset = get_dataset()
+    evaluate(dataset, log_prefix)
+
+def update_and_ensure_model_output_path(mode, index):
+    model_short_description = get_model_short_description()
+    model_source_code_path = get_model_source_code_path()
+    model_configuration = configuration.ensure_submodule('model')
+
+    @nmt.common.configured('data')
+    def get_train_dataset_title(train_root_path: str = './data/train'):
+        if os.path.isdir(train_root_path):
+            result = train_root_path
+        else:
+            result = os.path.dirname(train_root_path)
+        if result[-1] == '/':
+            result = result[:-1]
+        result = os.path.basename(result)
+        if result == '.' or result == '':
+            result = 'UNKNOWN'
+        return result
+
+    base_output_path = os.path.join(model_configuration.output_path, get_train_dataset_title())
+    if model_short_description is not None:
+        base_output_path = os.path.join(base_output_path, f'{model_short_description}')
+
+    if mode != 'train':
+        if not os.path.exists(base_output_path):
+            raise IOError(f'Path `{base_output_path}` does not exist!')
+        if index is None:
+            index = 1
+            while not os.path.exists(f'{base_output_path}/{index:03}'):
+                index += 1
+        model_configuration.output_path = f'{base_output_path}/{index:03}'
+    else:
+        index = 1
+        while os.path.exists(f'{base_output_path}/{index:03}'):
+            index += 1
+        model_configuration.output_path = f'{base_output_path}/{index:03}'
+        os.makedirs(model_configuration.output_path, exist_ok=True)
+        shutil.copyfile(
+            model_source_code_path,
+            os.path.join(model_configuration.output_path, 'model.py')
+        )
+        shutil.copyfile(
+            nmt.common.args.config_path,
+            os.path.join(model_configuration.output_path, 'config.json')
+        )
+
+def main(cli_args: Sequence[str]):
+
+    parser = argparse.ArgumentParser('nmt')
+    parser.add_argument(
+        '-m',
+        '--mode',
+        choices=[
+            'save_config', 'prepare_data', 'train', 'test', 'translate',
+            'sanity_check'
+        ],
+        help='Mode of execution.',
+        required=True
+    )
+    parser.add_argument(
+        '-x',
+        '--index',
+        type=int,
+        help='Model index to be used in case of "test/translate" modes.',
+        required=False
+    )
+    parser.add_argument(
+        '-c',
+        '--config_path',
+        type=str,
+        help='Path to the JSON formatted config file.',
+        required=True
+    )
+    parser.add_argument(
+        '-i',
+        '--input',
+        type=str,
+        help='Path to input text file.',
+        required=False
+    )
+    parser.add_argument(
+        '-o',
+        '--output',
+        type=str,
+        help='Path to output hypotheses file.',
+        required=False
+    )
+    parser.add_argument(
+        '-t',
+        '--type',
+        type=str,
+        help='Model type. Overrides configuration file.',
+        required=False
+    )
+    parser.add_argument(
+        '-v',
+        '--visualize',
+        action='store_true',
+        help='Enable/disables visualization of model artifcats.',
+        required=False
+    )
+    parser.add_argument(
+        '-b',
+        '--base_output_path',
+        type=str,
+        help='Enable/disables visualization of model artifcats.',
+        required=False
+    )
+
+    args = parser.parse_args(cli_args)
+    nmt.common.args = args
+
+    set_mode(args.mode)
+
+    if args.mode != 'save_config':
+        with open(args.config_path) as f:
+            configuration.load(json.load(f))
+        if args.type is not None:
+            configuration.ensure_submodule('model').type = args.type
+        if args.mode != 'translate' or not args.base_output_path:
+            update_and_ensure_model_output_path(args.mode, args.index)
+        else:
+            configuration.ensure_submodule('model').output_path = args.base_output_path
+
+        make_logger()
+
+    if args.visualize:
+        visualization.enabled = True
+
+    if args.mode == 'save_config':
+        with open(args.config_path, 'w') as f:
+            json.dump(configuration.get_as_dict(), f, indent=4)
+    elif args.mode == 'prepare_data':
+        prepare_data()
+    elif args.mode == 'train':
+        train()
+    elif args.mode == 'test':
+        run_evaluate(get_validation_dataset, 'validation')
+        run_evaluate(get_test_dataset, 'test')
+    elif args.mode == 'translate':
+        if not args.input or not args.output:
+            raise ValueError('In order to use translate command, both input and output path must be specified.')
+        predict(args.input, args.output, 'translate')
+    elif args.mode == 'sanity_check':
+        sanity_check()
+    else:
+        raise ValueError('Invalid execution mode.')
---
+++ scripts/evaluate_models.py
@@ -0,0 +1,141 @@
+# coding: utf-8
+"""
+Script to evaluate all models trained on a given dataset
+"""
+
+from nmt import model
+import os
+import sys
+import json
+import glob
+import argparse
+from importlib import util as iu
+
+try:
+    nmt_module_path = '.'
+    import nmt
+except:
+    sys.path.insert(0, '..')
+    nmt_module_path = '..'
+    import nmt
+
+def main():
+
+    parser = argparse.ArgumentParser('nmt')
+    parser.add_argument(
+        '-b',
+        '--base_path',
+        help='Base path to trained models',
+        required=True
+    )
+    parser.add_argument(
+        '-c',
+        '--config_path',
+        help='Path to the base config file. Model type will be overrided.',
+        required=False
+    )
+
+    args = parser.parse_args()
+
+    MODEL_KEY = 'nmt.mtmodel'
+    MODEL_DICT = {}
+    def load_model_module(file_path: str, short_description: str = None, store_in_model_dict: bool = True):
+        if (file_path not in MODEL_DICT) or not store_in_model_dict:
+            module_spec = iu.spec_from_file_location(MODEL_KEY, file_path)
+            if module_spec is None:
+                raise Exception(
+                    f'Error loading `{file_path}` file.'
+                )
+            model_module = iu.module_from_spec(module_spec)
+            module_spec.loader.exec_module(model_module)
+            if hasattr(model_module.Model, 'short_description'):
+                short_description = model_module.Model.short_description()
+            else:
+                print(f'Warning! Model in {file_path} has no `short_description`.')
+            if short_description is None:
+                print(f'Warning! Populated `short_description` is `None`.')
+                short_description, _ = os.path.splitext(os.path.basename(file_path))
+            print(f'Registering {short_description} <= {file_path}')
+            if not store_in_model_dict:
+                return model_module
+            MODEL_DICT[file_path] = (short_description, model_module)
+
+    def gather_available_models():
+        for file_path in glob.glob(f'{nmt_module_path}/nmt/models/**/*.py'):
+            load_model_module(file_path)
+    gather_available_models()
+
+    for model_path in glob.glob(f'{args.base_path}/**/*'):
+        print(f'Model in {model_path}')
+        print('***********************************************************')
+
+        model_file_path = None
+        config_file_path = None
+
+        for file_path in glob.glob(f'{model_path}/*.py'):
+            model_file_path = file_path
+            break
+        for file_path in glob.glob(f'{model_path}/*.json'):
+            config_file_path = file_path
+            break
+
+        if config_file_path is None:
+            config_file_path = args.config_path
+
+        if not config_file_path:
+            print(f'Warning! No configuration could be inferred.')
+            continue
+
+        if model_file_path is None:
+            path_parts = model_path.split('/')
+            index = None
+            try:
+                index = int(path_parts[-1])
+                model_type = path_parts[-2]
+            except:
+                model_type = path_parts[-1]
+            for file_path, (short_description, _) in MODEL_DICT.items():
+                if short_description == model_type:
+                    model_file_path = file_path
+                    break
+            if model_file_path is None:
+                for file_path, (short_description, _) in MODEL_DICT.items():
+                    if model_type in file_path:
+                        model_file_path = file_path
+                        break
+        if model_file_path is None:
+            print(f'Skipping `{model_type}`, model implementation not found.')
+            continue
+
+        if model_file_path in MODEL_DICT:
+            sys.modules[MODEL_KEY] = MODEL_DICT[model_file_path][-1]
+        else:
+            sys.modules[MODEL_KEY] = load_model_module(model_file_path, None, False)
+
+        with open(config_file_path) as f:
+            configuration = json.load(f)
+
+        input_file_path = f"{configuration['data']['test_root_path']}.{configuration['data']['src_lang_code']}"
+        output_file_path = os.path.join(model_path, os.path.basename(configuration['data']['test_root_path']))
+        output_file_path = f"{output_file_path}.hypo.{configuration['data']['tgt_lang_code']}"
+
+        print(f'BASE CONFIG. FILE: {config_file_path}')
+        print(f'INPUT FILE: {input_file_path}')
+        print(f'OUTPUT FILE: {output_file_path}')
+
+        nmt.cli_interface.main([
+            '-m',
+            'translate',
+            '-c',
+            config_file_path,
+            '-i',
+            input_file_path,
+            '-o',
+            output_file_path,
+            '-t',
+            'mtmodel',
+            '-b',
+            model_path,
+        ])
+if __name__ == '__main__':
+    main()
